01d2a1936ca83f40beb6ec2efb333a770d8cfa73
Israel <isreldouglas25@gmail.com>
2023-11-08 17:21:52 -0300

    feat: create UserService

---
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuario do tipo lojista não autorizado a realizar transação.");
+        }
+        if (sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente!");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuario não encontrado"));
+    }
+
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+}
